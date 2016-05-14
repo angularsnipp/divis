dc3d042375be492f11cf2d944e72339c775133dc
krispo <konstantin.skipor@gmail.com>
2016-05-14 21:40:26 +0300

    update logger

--- examples/script.js
+++ examples/script.js
@@ -7,7 +7,16 @@ function generate(n, f){
   return arr;
 }
 
-function logEvent(element, text){
+function logEvent(element, _, text){
+  if (_) {
+    text =
+      '<div class="log-event">'+ _.event +'</div>' +
+      '<div class="log-data">data: <pre>'+ JSON.stringify(_.d, null, 2) +'</pre></div>' +
+      '<div class="log-index">index: '+ _.i +'</div>';
+
+    if (_.hasOwnProperty('s'))
+      text += '<div class="log-series-index">series index: '+ _.s +'</div>';
+  }
   var log = document.getElementById(element);
   var div = document.createElement('div');
   div.innerHTML = text;
